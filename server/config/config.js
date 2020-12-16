79764a2be38d640769f5569da8daf6b7ab8493eb
francoandes12 <francoandes12@gmail.com>
2020-12-15 23:53:42 -0300

    Rest server- Usuarios fix

--- server/config/config.js
+++ server/config/config.js
@@ -5,9 +5,9 @@ process.env.PORT = process.env.PORT || 3000
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 let urlDb
 /* BASE DE DATOS */
-/* if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
   urlDb = 'mongodb://localhost:27017/cafe'
-} else { */
-urlDb = 'mongodb+srv://franco:1207@chat-andes.vvqwb.mongodb.net/cafe'
-
+} else {
+  urlDb = 'mongodb+srv://franco:1207@chat-andes.vvqwb.mongodb.net/cafe'
+}
 process.env.URLDB = urlDb
